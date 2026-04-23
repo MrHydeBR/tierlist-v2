b62f018371fe051991be4620da1a9fa20228d467
Mauro Ribeiro (VPN) <mauro.ribeiro@venhapranuvem.com.br>
2026-04-23 11:01:47 -0300

    Add build script for playwright

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Sair se houver erro
+set -o errexit
+
+echo "--- Instalando dependências do Python ---"
+pip install -r requirements.txt
+
+echo "--- Instalando navegadores do Playwright ---"
+python -m playwright install --with-deps chromium
+
+echo "--- Build finalizado com sucesso ---"
